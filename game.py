db79ab5533bab736ef68f171092e82de817a0f32
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-03-23 16:10:49 +0100

    added player on screen

--- game.py
+++ game.py
@@ -2,24 +2,43 @@
 """
 a file with game class
 """
-import settings
+import settings,player
 import pygame
 import sys
 
 class Game:
     
-    #init method called when game starts
+    #init method
     def __init__(self):
         self.gameON = True
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((settings._WIDTH, settings._HEIGHT))
+        self.newGame()
     
+    #main loop of the game
     def play(self):
         while self.gameON:
-            self.manageInput()
+            #draw elements
+            self.drawElements()
+            #show elements
             pygame.display.update()
 
+            self.manageInput()
+            
+            self.clock.tick(60)
+
+    #method to manage player input
     def manageInput(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+    
+    def drawElements(self):
+        self.screen.fill((255,255,255))
+        self.P1.blit()
+        
+
+    #make new game
+    def newGame(self):
+        self.P1 = player.Player(self.screen)
--- player.py
+++ player.py
@@ -2,3 +2,25 @@
 """
 file with player class
 """
+import pygame
+import settings
+
+class Player:
+
+    #init method
+    def __init__(self,screen):
+        self.newPlayer()
+        self.screen = screen
+    
+    def newPlayer(self):
+        #player starts with 0 points
+        self.score = 0
+        self.image = pygame.Surface((40,40))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = settings._WIDTH//2
+        self.rect.bottom = settings._HEIGHT - 20
+    
+    def blit(self):
+        self.screen.blit(self.image,self.rect)
+
